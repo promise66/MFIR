01a48e61ddb016c33a741cbc4d8a31ec4a169d5b
promise66 <38325179+promise66@users.noreply.github.com>
2026-03-23 14:19:50 +0800

    Add files via upload

---
+++ ccode/debug.h
@@ -0,0 +1,17 @@
+/*	Copyright © 2015 Alexander Isakov. Contact: <alexander.isakov@tuhh.de>
+ *	Copyright © 2015 Marina Krotofil. Contact: <marina.krotofil@tuhh.de>
+ *	Copyright © 2015 TUHH-SVA Security in Distributed Applications.
+ * 	All rights reserved.
+ *	License: http://opensource.org/licenses/BSD-3-Clause
+ *	---------------------------------------------------------------------
+ */	
+
+#ifndef __DEBUG__
+#define __DEBUG__
+
+#define debug(...) \
+	#ifdef DEBUG \
+	ssPrintf(__VA_ARGS__) \
+	#endif
+
+#endif /* __DEBUG__ */
